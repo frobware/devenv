48a1de611151658464fa675b97655ccdd030d7f5
Andrew McDermott <aim@frobware.com>
2019-09-27 09:35:39 +0100

    Add guru

--- gotools/tools.bash
+++ gotools/tools.bash
@@ -6,3 +6,4 @@ source $TOPDIR/env.bash
 go get ${UPDATE} github.com/rogpeppe/godef
 go get ${UPDATE} github.com/mdempsky/gocode
 go get ${UPDATE} golang.org/x/tools/cmd/goimports
+go get ${UPDATE} golang.org/x/tools/cmd/guru
